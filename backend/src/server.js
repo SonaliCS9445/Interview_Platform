90ef034dfb0323fa21adfca6c9604bc1240b8d94
sonali <sonalik.ug23.cs@nitp.ac.in>
2026-02-09 19:16:52 +0530

    deploy_seperately

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import path from 'path';
-import { ENV } from './lib/env.js';
+import mongoose from "mongoose";
+import cors from "cors";
 
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
-const app = express()
-
-const __dirname = path.resolve();
+mongoose.connect(process.env.DB_URI)
+  .then(() => console.log("DB connected"))
+  .catch(() => console.log("DB not connected yet"));
 
 app.get('/health', (req, res) => {
   res.status(200).json({message: "success from api"})
@@ -16,16 +19,9 @@ app.get('/books', (req, res) => {
   res.status(200).json({message: "this is the books api"})
 });
 
-//make our app ready for development
-if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+const PORT = process.env.PORT || 5000;
 
-    app.get("/{*any}", (req, res) => {
-          res.sendFile(path.join(__dirname, '../frontend/dist/index.html')); 
-    })
-}
-
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+});
 
