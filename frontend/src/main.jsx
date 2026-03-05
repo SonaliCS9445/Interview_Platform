1c45847c67b08c5f573fbef7161359881f600760
sonali <sonalik.ug23.cs@nitp.ac.in>
2026-03-05 20:05:27 +0530

    Home page create

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,6 +16,7 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
+console.log(PUBLISHABLE_KEY);
 
 const queryClient = new QueryClient();
 
